08eeb3fef40ddbf5f2be2f95fb95a6d5bc8722dc
Srikanth <srikanthg@chromium.org>
2019-03-27 05:36:21 -0400

    Added Insert and Print features

--- LinkedLists/LinkedList.py
+++ LinkedLists/LinkedList.py
@@ -1,4 +1,27 @@
-class Node:
-    def __init__(self, value, next=None):
+class LinkedList:
+    def __init__(self, value=None, nextNode=None):
         self.value = value
-        self.next = next
+        self.nextNode = nextNode
+
+    def insertNodeAtStart(self, value):
+        #print "Insert Node at the begning"
+        head = LinkedList(value)
+        head.nextNode = self
+        return head;
+
+    def insertNodeAtEnd(self):
+        print "Insert Node at the end"
+    def printLinkedList(self, list):
+        print "Printing the Linked List"
+        while list.nextNode != None:
+            print list.value
+            list = list.nextNode
+
+def main():
+    print "Inside main"
+    list = LinkedList()
+    for i in range(0, 10, 1):
+        list = list.insertNodeAtStart(i)
+    list.printLinkedList(list)
+
+main()
